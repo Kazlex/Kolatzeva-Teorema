e57fbb6a91a32fd7b15b41278fa7585bd2c1f22b
Armin <Armin@DESKTOP-LVFB4U7>
2022-01-20 14:56:30 +0100

    Add project files.

---
+++ Brojevi do 1/Program.cs
@@ -0,0 +1,35 @@
+﻿do
+{
+    int broj;
+    do
+    {
+        Console.WriteLine("Unesite broj");
+        broj = int.Parse(Console.ReadLine());
+        if (broj <= 1)
+        {
+            Console.WriteLine("ERROR");
+            break;
+        }
+        do
+        {
+            do
+            {
+                if (broj % 2 == 0)
+                {
+                    broj = broj / 2;
+                    Console.WriteLine(broj);
+                    break;
+                }
+                if (broj % 2 == 1)
+                {
+                    broj = broj * 3 + 1;
+                    Console.WriteLine(broj);
+                }
+            }
+            while (true);
+        }
+        while (broj > 1);
+    }
+    while (broj > 1);
+} while (true); 
+while (true);
